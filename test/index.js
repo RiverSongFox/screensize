477bdf1e1821ad127f045837f3f9e16d9e11a546
Nikolay Kim <mail@yaruson.ru>
2015-12-15 11:52:09 +0300

    Library commit
    Initial library code + unit tests

--- test/index.js
+++ test/index.js
@@ -3,8 +3,17 @@
 var assert = require('assert');
 var screensize = require('../lib');
 
+function runTest(diagonal, resX, resY, width, height, precision) {
+  var s = screensize.calculate(diagonal, resX, resY);
+  assert.equal(width.toPrecision(precision), s.width.toPrecision(precision));
+  assert.equal(height.toPrecision(precision), s.height.toPrecision(precision));
+}
+
 describe('screensize', function () {
-  it('should have unit test!', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
+  it('Nokia 105', function () {
+    runTest(1.36, 68, 96, 0.78610, 1.10979, 5);
+  });
+  it('Huawei Nexus 6P', function () {
+    runTest(5.70, 2560, 1440, 4.96798, 2.79449, 5);
   });
 });
